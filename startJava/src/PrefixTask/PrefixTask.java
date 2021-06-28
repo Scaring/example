646f2572831bcae87cc5dc76e226785af882b285
scaringHome <scaring22@gmail.com>
2021-06-28 13:59:55 +0200

    task -prefix- compleated

--- startJava/src/PrefixTask/PrefixTask.java
+++ startJava/src/PrefixTask/PrefixTask.java
@@ -4,28 +4,30 @@ class PrefixTask {
     public static String prefix = "";
 
     public static void main(String[] args) {
-        String[] task = {"task1", "task2", "task12113","task323411"}; 
+        String[] task = { "task1", "task2", "task12113", "task123411" };
 
-
-
-        for ( int i = 0; i < task.length; i++ ){
-            if ( i == (task.length -1)) break;
+        for (int i = 0; i < task.length; i++) {
+            if (i == (task.length - 1))
+                break;
 
             String curPrefix = "";
             String curentWord = task[i];
-            String nextWord = task[ i+1 ];
+            String nextWord = task[i + 1];
             // System.out.println("Curent " + curentWord + " and next " + nextWord);
 
-            for( int j = 0; j < curentWord.length(); j++){
-                if(curentWord.charAt(j) != nextWord.charAt(j)) break;
-                if(curentWord.charAt(j) == nextWord.charAt(j)) curPrefix += curentWord.charAt(j);
+            for (int j = 0; j < curentWord.length(); j++) {
+                if (curentWord.charAt(j) != nextWord.charAt(j))
+                    break;
+                if (curentWord.charAt(j) == nextWord.charAt(j))
+                    curPrefix += curentWord.charAt(j);
             }
-            
-            if ( !prefix.equals("") || prefix.equals(curPrefix) ) break;
+
+            if (!prefix.equals("") || prefix.equals(curPrefix))
+                break;
 
             prefix = curPrefix;
         }
 
         System.out.println(prefix);
-     }
+    }
 }
---
+++ startJava/src/PrefixTask/PrefixTaskMy.java
@@ -0,0 +1,44 @@
+package PrefixTask;
+
+//task: "Find the same beginings of all strings in array"
+
+class PrefixTaskMy {
+    public static String prefix = "";
+
+    public static void main(String[] args) {
+        String[] task = { "task1", "task2", "task12113", "task123411" };
+
+        for (int i = 2; i < task.length; i++) {
+
+            if (i == 2) {
+                prefix = PrefixOfComparedWords(task[0], task[1]);
+            }
+
+            prefix = PrefixOfComparedWords(prefix, task[i]);
+
+            if (prefix == "") {
+                System.out.println("No prefix of array strings!");
+                break;
+            }
+
+        }
+
+        System.out.println(prefix);
+    }
+
+    public static String PrefixOfComparedWords(String firstWord, String secondWord) {
+        String twoWordsPrefix = "";
+
+        for (int j = 0; j < firstWord.length(); j++) {
+
+            if (firstWord.charAt(j) != secondWord.charAt(j))
+                break;
+
+            if (firstWord.charAt(j) == secondWord.charAt(j))
+                twoWordsPrefix += firstWord.charAt(j);
+
+        }
+
+        return twoWordsPrefix;
+    }
+}
