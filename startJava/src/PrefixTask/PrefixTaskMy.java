943cf1509583a671cd2d359d68c7484b57a9ba7f
scaringHome <scaring22@gmail.com>
2021-06-28 14:32:30 +0200

    -prefix- updated method

--- startJava/src/PrefixTask/PrefixTaskMy.java
+++ startJava/src/PrefixTask/PrefixTaskMy.java
@@ -8,22 +8,21 @@ class PrefixTaskMy {
     public static void main(String[] args) {
         String[] task = { "task1", "task2", "task12113", "task123411" };
 
+        prefix = PrefixOfComparedWords(task[0], task[1]);
+
         for (int i = 2; i < task.length; i++) {
-
-            if (i == 2) {
-                prefix = PrefixOfComparedWords(task[0], task[1]);
-            }
-
-            prefix = PrefixOfComparedWords(prefix, task[i]);
 
             if (prefix == "") {
                 System.out.println("No prefix of array strings!");
                 break;
             }
 
+            prefix = PrefixOfComparedWords(prefix, task[i]);
+
         }
 
-        System.out.println(prefix);
+        if (prefix != "")
+            System.out.println(prefix);
     }
 
     public static String PrefixOfComparedWords(String firstWord, String secondWord) {
