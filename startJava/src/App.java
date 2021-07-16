d9046c83368757751db61d83de36b44aa4fef6b9
Dmytro Kuschevyy <scaring22@gmail.com>
2021-07-16 21:17:56 +0300

    test commit2

--- startJava/src/App.java
+++ startJava/src/App.java
@@ -1,7 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        boolean bool = 2 > 5;
-        System.out.println(bool = true);
-        args.le
+        System.out.println("nothing");
+
     }
 }
